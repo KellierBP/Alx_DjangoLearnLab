539c41a04b2aca0028fd7b8bd64c4beae83cfa9c
KellierBP <trevorkelllier@gmail.com>
2025-11-09 22:47:44 +0300

    urls fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView, register, admin_dashboard, librarian_dashboard, member_dashboard
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register, name='register'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html', next_page='list_books'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
     # Role-based URLs
-    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    path('librarian-dashboard/', views.librarian_dashboard, name='librarian_dashboard'),
-    path('member-dashboard/', views.member_dashboard, name='member_dashboard'),
+    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('librarian-dashboard/', librarian_dashboard, name='librarian_dashboard'),
+    path('member-dashboard/', member_dashboard, name='member_dashboard'),
 ]
